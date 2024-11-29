74ea8861954cc79a8f82ae3690b29645fc8a0ece
Alejandro Gaona <aleg@localhost.localdomain>
2024-11-29 01:22:08 -0300

    Inicio del backend, request básica de carreras.

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
-
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 const app = express ();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const careerRoutes = require("./routes/careerRoutes.js");
+
+app.use('/', careerRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Testeando.');
+});
 
 app.listen(PORT, () => {
     console.log("Server Listening on http://localhost:" + PORT);
